b0adb7631157c474bc44fc75fd804b3a67255995
luochongqi <luo760430169@163.com>
2023-06-12 14:39:22 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+# universe_age = 1_4555_2331
+# print(universe_age)
+#
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[0].title())
+# message = f"My first bicycle was a {bicycles[0].title()}."
+# print(message)
+
+# guest = [ 'xiaoming', 'xiaohong', 'xiaolv', 'xiaolan']
+# print(guest)
+# print(f"{guest[0].title()} is so busy, so he can't arrive")
+# del guest[0]
+# print(guest)
+# guest.insert(0, 'xiaoju')
+# print(guest)
+#
+# guest.append('peiqi')
+# guest.insert(0, 'aoteman')
+# guest.insert(int(len(guest)/2), 'dehua')
+# print(guest)
+#
+# away_guest = guest.pop()
+# warning = f"dear {away_guest}, sorry, you have to leave!"
+# print(warning)
+# away_guest = guest.pop(3)
+# warning = f"dear {away_guest}, sorry, you have to leave!"
+# print(warning)
+
+# print(guest)
+# del guest[0]
+# del guest[0]
+# del guest[0]
+# del guest[0]
+# del guest[0]
+# print(guest)
+
+students = ['luohaoye', 'tanxinzhen', 'zhanghuiyao', 'yijiayao', 'pengrenjie']
+# students.sort(reverse=True)
+# print(students)
